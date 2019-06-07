b7f906b312e85ff3368b933aa23734cdc13fe898
Vashist Nagori <vashistnagori@Vashists-MacBook-Air.local>
2019-06-07 17:09:47 -0500

    setting up Expense Sumary in expense dashboard component

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter';
+import {ExpenseSumary} from './ExpenseSumary';
+import {connect} from 'react-redux';
+import {expenseTotal} from '../selectors/expense-total';
 
-const ExpenseDashboard=(props)=>{
-    //console.log(props);
+
+const ExpenseDashboardMaking=(props)=>{
+const total= expenseTotal(props.expenses);
    return(<div>
     <p>this is homepage</p>
     <ExpenseListFilter />
+    <ExpenseSumary total={total} count={props.expenses.length}/>
     <ExpenseList />
     </div>);
 }
+
+const mapStateToProps =(state)=>({
+    expenses:state.expenses
+});
+
+const ExpenseDashboard= connect(mapStateToProps)(ExpenseDashboardMaking);
+
+
 export default ExpenseDashboard;
